8372358d417356e98597409c20c95ae290ff71bd
meredith-jones <meredithjw@gmail.com>
2016-08-23 14:00:05 -0700

    Added data-gathering code

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+def data_gather()
+  puts "What is your name?"
+  name = gets.chomp
+  puts "How old are you?"
+  age = gets.chomp
+  puts "What year were you born?"
+  birth_year = gets.chomp
+  puts "Do you like garlic bread? (y/n)"
+  garlic_preference = gets.chomp
+  puts "Would you like to enroll in the company’s health insurance? (y/n)"
+  health_insurance = gets.chomp
+end
