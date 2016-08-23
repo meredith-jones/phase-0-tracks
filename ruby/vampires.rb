ddce930e2526626da52325dc42088b589eea17c3
meredith-jones <meredithjw@gmail.com>
2016-08-23 15:02:54 -0700

    Added loop to address multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,29 +1,37 @@
-def data_gather()
-  puts "What is your name?"
-  name = gets.chomp
-  puts "How old are you?"
-  age = gets.chomp.to_i
-  puts "What year were you born?"
-  birth_year = gets.chomp.to_i
-  puts "Do you like garlic bread? (y/n)"
-  garlic_preference = gets.chomp
-  puts "Would you like to enroll in the company’s health insurance? (y/n)"
-  health_insurance = gets.chomp
+def employee_screening()
+  puts "How many employees need to be processed?"
+  num_employees = gets.chomp.to_i
 
-      if name == "Drake Cula" || name == "Tu Fang"
-        is_vamp = "Definitely a vampire"
-      elsif (age != 2016 - birth_year) && (garlic_preference == "n") && (health_insurance == "n")
-        is_vamp = "Almost certainly a vampire"
-      elsif (age != 2016 - birth_year) && ((garlic_preference == "n") || (health_insurance == "n"))
-        is_vamp = "Probably a vampire"
-      elsif (age == 2016 - birth_year) && ((garlic_preference == "y") || (health_insurance == "y"))
-        is_vamp = "Probably not a vampire"
-      else
-        is_vamp = "Results inconclusive"
-      end
-puts is_vamp
+  counter = 0
+  while counter < num_employees
+
+        puts "What is your name?"
+        name = gets.chomp
+        puts "How old are you?"
+        age = gets.chomp.to_i
+        puts "What year were you born?"
+        birth_year = gets.chomp.to_i
+        puts "Do you like garlic bread? (y/n)"
+        garlic_preference = gets.chomp
+        puts "Would you like to enroll in the company’s health insurance? (y/n)"
+        health_insurance = gets.chomp
+
+            if name == "Drake Cula" || name == "Tu Fang"
+              is_vamp = "Definitely a vampire"
+            elsif (age != 2016 - birth_year) && (garlic_preference == "n") && (health_insurance == "n")
+              is_vamp = "Almost certainly a vampire"
+            elsif (age != 2016 - birth_year) && ((garlic_preference == "n") || (health_insurance == "n"))
+              is_vamp = "Probably a vampire"
+            elsif (age == 2016 - birth_year) && ((garlic_preference == "y") || (health_insurance == "y"))
+              is_vamp = "Probably not a vampire"
+            else
+              is_vamp = "Results inconclusive"
+            end
+      puts "Survey results: #{is_vamp}."
+      counter += 1
+  end
 
 
 end
 
-data_gather()
+employee_screening()
