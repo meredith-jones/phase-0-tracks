3cf080e5cb066eb5705e2bbd577d620c5dec27dd
meredith-jones <meredithjw@gmail.com>
2016-08-25 13:53:32 -0700

    Added user interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -11,10 +11,10 @@ def encrypt(string)
     encrypted_word += string[index].next
     index += 1
   end
-  puts encrypted_word
+  p encrypted_word
 end
 
-encrypt("hello")
+# encrypt("hello")
 
 
 
@@ -42,4 +42,41 @@ def decrypt(string)
   puts decrypted_word
 end
 
-decrypt("ifmmp")
+# decrypt("ifmmp")
+
+
+# encrypt("abc")
+# encrypt("zed")
+# decrypt("bcd")
+# decrypt("afe")
+
+# decrypt(encrypt("swordfish"))
+#It works because they're nested properly nested with closing parenthesis
+
+#Interface
+# Ask user: decrypt or encrypt
+# Gets.chomp
+# If statement:
+#     if decrypt:
+#       run decrypt
+#     else
+#       run encrypt
+#     end
+# Ask for password
+#   gets.chomp
+#   runs appropriate method
+#   prints result
+
+#DRIVER CODE
+
+puts "would you like to encrypt or decrypt?"
+response = gets.chomp
+if response == "encrypt"
+  puts "enter the password:"
+  password = gets.chomp
+  encrypt(password)
+else
+  puts "enter the password:"
+  password = gets.chomp
+  decrypt(password)
+end
