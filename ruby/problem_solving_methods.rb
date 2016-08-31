2f67a185ecec1b1d577e6be6d92e556ca22f2686
meredith-jones <meredithjw@gmail.com>
2016-08-31 15:57:38 -0700

    Bubble sort method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,12 +1,10 @@
 #Hoa Huynh and Meredith Jones pairing
 
 #Release 0:
+
 #create a method that takes in array and integer
-
 #compare each element of the array with the integer.
-
 #return index if array element and integer match
-
 #return nil if nothing matches.
 
 def search_array(arr, int)
@@ -22,7 +20,8 @@ index = 0
   end
 end
 
-#Release 1
+
+#Release 1:
 
 def fibonacci(num)
   fib_nums = [0, 1] #initializing array
@@ -37,4 +36,28 @@ end
 fibonacci(100)
 
 
-#Release 2
+#Release 2:
+
+#Create method that takes in an array
+#Compare the first two elements, then the 2nd and 3rd element, and so forth, until the end of the array.
+#Swap from one element to the other if they are out of order.
+#Repeat that until no more swapping needs to take place
+#Return the final array
+
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+  array
+end
+
+arr = [34, 56, 2, 4, 8, 23]
+bubble_sort(arr)
