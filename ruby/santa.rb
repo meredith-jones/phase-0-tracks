ec1f4ce955ddef85c86deead839a2d5ec7240e12
meredith-jones <meredithjw@gmail.com>
2016-09-06 18:10:45 -0700

    Added getter and setter methods.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,12 +1,11 @@
 
 class Santa
 
-  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-  @age = 0
-
   def initialize(gender, ethnicity)
     @gender = gender
     @ethnicity = ethnicity
+    @age = 0
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     puts "Initializing Santa instance..."
   end
 
@@ -14,8 +13,38 @@ class Santa
     puts "Ho, ho, ho! Haaaapy holidays!"
   end
 
+  def tell_demographic
+      puts "Hi, my gender is #{@gender} and my ethnicity is #{@ethnicity}"
+  end
+
   def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie} cookie!"
+  end
+#getter methods:
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+#attribute-changing methods:
+  def celebrate_birthday
+    @age += 1
+    puts "Santa is #{@age} now!"
+  end
+
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking << reindeer
+    p @reindeer_ranking
+  end
+
+#setter method:
+  def gender=(new_gender)
+    @gender = new_gender
+    p @gender
   end
 
 end
@@ -24,10 +53,17 @@ end
 #DRIVER CODE
 santah = Santa.new("male", "Italian")
 santah.speak
+santah.celebrate_birthday
+santah.celebrate_birthday
+santah.get_mad_at("Prancer")
+santah.get_mad_at("Dasher")
+santah.gender=("female")
+
 
 clause = Santa.new("female", "Chinese")
 clause.eat_milk_and_cookies("Oatmeal Raisin")
 
+#create array of santa instances
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
@@ -38,37 +74,6 @@ end
 #loop through and print the santas
 puts "Iterating through Santas..."
 santas.each do |santa|
-  puts "#{santa} says: #{santa.speak}"
+  puts santa.tell_demographic
 end
 
-#Reindeer example
-#instance variable has @ sign in front of it
-
-# class Reindeer
-#   def initialize(name)
-#     @name = name
-#     @location = "The North Pole"
-#   end
-
-#   def take_off(altitude)
-#     puts "#{@name} took off."
-#     puts "#{@name} ascended to #{altitude} feet."
-#   end
-
-#   def land(location)
-#     puts "Landed safely in #{location}."
-#     @location = location
-#   end
-
-#   def about
-#     puts "Name: #{@name}"
-#     puts "Location: #{@location}"
-#   end
-
-# end
-
-# reindeer = Reindeer.new("Blitzen")
-# reindeer.take_off(3000)
-# reindeer.about
-# reindeer.land("Bombay")
-# reindeer.about
