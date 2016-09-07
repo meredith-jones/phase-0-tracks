580efad4811d63b7409b6e71ace24b93cbe723bc
meredith-jones <meredithjw@gmail.com>
2016-09-07 14:36:30 -0700

    Added 2 yelling methods to Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,15 @@
 module Shout
 
+  def self.yell_angrily(words)
+      words.upcase + "!!!" + " >:("
+  end
+
+  def self.yelling_happily(words)
+      words + "!!!" + " <3 " + " :)"
+  end
+
 end
+
+#DRIVER CODE:
+puts Shout.yelling_happily("You're the best")
+puts Shout.yell_angrily("Eat a bag of dicks")
