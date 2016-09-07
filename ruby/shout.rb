dc9ebaf0998f68e8123f2c568d06af6064c49b09
meredith-jones <meredithjw@gmail.com>
2016-09-07 15:00:53 -0700

    Changed the standalone shout module to a mixin, created 2 classes to use the mixin. Added driver code to test the mixin/classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,47 @@
+# MIXIN VERSION OF SHOUT MODULE:
 module Shout
 
-  def self.yell_angrily(words)
+  def yell_angrily(words)
       words.upcase + "!!!" + " >:("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
       words + "!!!" + " <3 " + " :)"
   end
 
 end
 
-#DRIVER CODE:
-puts Shout.yelling_happily("You're the best")
-puts Shout.yell_angrily("Eat a bag of dicks")
+# Classes that need shout module:
+class NewYorker
+    include Shout
+end
+
+class Cheerleader
+    include Shout
+end
+
+# DRIVER CODE:
+new_yorker = NewYorker.new
+puts new_yorker.yell_angrily("Fuhget about it")
+
+cheerleader = Cheerleader.new
+puts cheerleader.yelling_happily("Go team")
+
+
+# #STANDALONE MODULE:
+
+# module Shout
+
+#   def self.yell_angrily(words)
+#       words.upcase + "!!!" + " >:("
+#   end
+
+#   def self.yelling_happily(words)
+#       words + "!!!" + " <3 " + " :)"
+#   end
+
+# end
+
+# #DRIVER CODE for standalone module:
+# puts Shout.yelling_happily("You're the best")
+# puts Shout.yell_angrily("Eat a bag of dicks")
