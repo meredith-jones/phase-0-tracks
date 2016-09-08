350a37c2dd9439e07d4244fb6bc26a69ee15931a
meredith-jones <meredithjw@gmail.com>
2016-09-08 14:23:24 -0700

    Added class to pass rspecs

---
+++ ruby/list/to_do_list.rb
@@ -0,0 +1,25 @@
+class TodoList
+
+    attr_accessor :list
+
+    def initialize(array = [])
+      @list = array
+    end
+
+    def add_item(item)
+      @list << item
+    end
+
+    def get_item(index)
+      @list[index]
+    end
+
+    def get_items
+      @list
+    end
+
+    def delete_item(item)
+      @list.delete(item)
+    end
+
+end
