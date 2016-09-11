08e06ac5aeef12d70d0a3eeaa49aa863f4bdcb41
meredith-jones <meredithjw@gmail.com>
2016-09-10 19:11:45 -0700

    Added more tests and methods to game.

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,12 +1,36 @@
+class GuessMyWord
+
+    attr_accessor :secret_word, :letters, :guesses_left, :letter_guess, :board, :letters_guessed, :repeat_guess
+
 # Initialize method: takes a seret word inputted by player 1.
     # Initializes an array to contain letters guessed.
     # Initializes the number of guesses Player 2 has to guess the secret word.
 
+    def initialize(secret_word)
+      @secret_word = secret_word
+      @letters_guessed = []
+      @guesses_left = @secret_word.length * 2
+    end
+
 # Game board method: takes secret word, and creates a "board" with a blank space for every letter of the secret word.
     # Will return the board (the string of blank spaces)
 
+    def game_board
+      @letters =  @secret_word.split('')
+      @board = []
+      @letters.map{| letter | @board << " _ "}
+      p @board.join
+    end
+end
+
 # Method for repeat letter guess: runs if user guesses the same letter a 2nd time.
     # Returns a string indicating how many guesses they have left.
+      def repeat_guess(letter_guess)
+        @letter_guess = letter_guess
+        puts "Letters you've guessed thus far:"
+        p @letters_guessed
+        "You already guessed '#{@letter_guess}'. Guess another letter. You have #{@guesses_left} guesses left."
+      end
 
 # Method for correct letter guess: intakes a letter that has been identified as included in the secret word.
     # Decrements letters guessed. Adds that letter to the array of already guessed letters.
@@ -15,6 +39,65 @@
         # IF so, displays winning message,
         # ELSE displays letters guessed so far and guesses remaining.
 
+    def correct_guess(letter_guess)
+      @letter_guess = letter_guess
+      @letters_guessed << @letter_guess
+      @guesses_left -= 1
+      #find index of letter on secret word
+      indices = @letters.each_index.select { |i| @letters[i] == @letter_guess}
+      #put the letter on same index of game board
+      indices.each{|i| @board[i] = @letter_guess}
+      #display game board
+      p @board.join
+          if @secret_word == @board.join
+              puts "That's right! It's #{@secret_word}!!"
+              puts "<3 You win! You're awesome!!! <3"
+          else
+              puts "Letters you've guessed thus far:"
+              p @letters_guessed
+              puts "Excellent! You have #{@guesses_left} guesses left."
+          end
+      end
+
+     # method for incorrect letter guess
+     def incorrect_guess(letter_guess)
+      @letter_guess = letter_guess
+      @letters_guessed << @letter_guess
+      puts "Letters you've guessed thus far:"
+      p @letters_guessed
+      @guesses_left -= 1
+      puts "There is no '#{@letter_guess}' in the secret word! You have #{@guesses_left} guesses left."
+    end
+
+end
 # Method for incorrect letter guess: intakes a letter that has been identified as not included in the secret word.      # Adds that letter to the array of letters guessed.
       # Decrements letters guessed.
       # Displays a message saying the letter is not in the secret word, and how many guesses remain.
+
+
+#DRIVER CODE:
+
+puts "Welcome to the Word Guessing Game"
+puts "Player 1: Enter a word!"
+secret_word = gets.chomp
+30.times{puts "*"}
+game = GuessMyWord.new(secret_word)
+game.game_board
+
+puts "Player 2: Guess a letter. You have #{game.guesses_left} guesses!"
+
+while game.guesses_left >= 0
+      letter_guess = gets.chomp
+      if game.letters_guessed.include?(letter_guess)
+          puts "Letters you've guessed thus far:"
+          puts game.letters_guessed
+          puts "You already guessed '#{letter_guess}'. Guess another letter. You have #{game.guesses_left} guesses left."
+      elsif secret_word.include?(letter_guess)
+        game.correct_guess(letter_guess)
+      else
+        game.incorrect_guess(letter_guess)
+      end
+    break if game.board.join == secret_word
+end
+
+puts "You lose. You should work on your skills." if game.board.join != secret_word
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -3,9 +3,12 @@ require_relative 'game'
 describe 'GuessMyWord' do
   let(:game) { GuessMyWord.new("hello") }
 
-  it "stores the list items given on initialization" do
-    expect(game.game_board).to eq " _  _  _  _  _"
+  it "creates game board based on user input" do
+    expect(game.game_board).to eq " _  _  _  _  _ "
   end
 
+  it "notifies user of repeat letter guess and prompts user to guess a different letter" do
+    expect(game.repeat_guess).to end_with('guesses left.')
+  end
 
 end
