6dbd04bf4f3c8d9c05f9f0a70f1366d48808372d
meredith-jones <meredithjw@gmail.com>
2016-09-12 12:34:57 -0700

    I have no idea what is going on at the moment.

---
+++ puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Legs: 4
+# Friendly: yes
+# Adorable: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+
+
+# BEHAVIOR -----------------------
+# Run
+# Play
+# Poop
