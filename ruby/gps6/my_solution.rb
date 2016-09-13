49f99f6d1068e3fd8328810adccd7447731cf536
meredith-jones <meredithjw@gmail.com>
2016-09-12 18:27:29 -0700

    Code Refactor.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,7 +1,7 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge with: Sierra Mclawhorn.
+# We spent [1.5] hours on this challenge.
 
 # EXPLANATION OF require_relative
 # require_relative allows you access another file within the same directory
@@ -41,6 +41,16 @@ class VirusPredictor
       number_of_deaths = (@population * 0.05).floor
     end
 
+#Attempting another solution:
+#     if @population_density < 50
+#       number_of_deaths = (@population * 0.05).floor
+#     elsif @population_density > 200
+#       number_of_deaths = (@population * 0.04).floor
+#     else
+#       multiplier = (@population_density/50 * 0.1)
+#       number_of_deaths = (@population * multiplier).floor
+# end
+
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
@@ -51,17 +61,11 @@ class VirusPredictor
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
-    end
+    speed += 0.5 if @population_density >= 200
+    speed += 1 if @population_density >= 150
+    speed += 1.5 if @population_density >= 100
+    speed += 2 if @population_density >= 50
+    speed += 2.5 if @population_density < 50
 
     puts " and will spread across the state in #{speed} months.\n\n"
 
