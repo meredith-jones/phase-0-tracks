6e23ed68fc68e83e56ea544ebb833c638c8ff119
meredith-jones <meredithjw@gmail.com>
2016-09-21 16:39:53 -0700

    Added function to view symptoms from specific date.

--- databases/project/symptom_tracker.rb
+++ databases/project/symptom_tracker.rb
@@ -26,10 +26,34 @@ $db.execute(create_table_cmd)
 # db.execute("INSERT INTO symptoms (symptom, date, time_of_day, severity) VALUES ('fatigue', '9-20-16', 'night', 7)")
 
 
-
-
 def add_symptom(db, symptom, date, time_of_day, severity)
   $db.execute("INSERT INTO symptoms (symptom, date, time_of_day, severity) VALUES (?, ?, ?, ?)", [symptom, date, time_of_day, severity])
+end
+
+def view_all_symptoms
+  symptoms = $db.execute("SELECT * FROM symptoms;")
+    symptoms.each do | symptom |
+      puts "#{symptom['id']}."
+      puts "Symptom: #{symptom['symptom']}"
+      puts "Date: #{symptom['date']}"
+      puts "Time of day: #{symptom['time_of_day']}"
+      puts "Severity: #{symptom['severity']}"
+      puts "________________________________"
+    end
+end
+
+def view_symptoms_time_of_day(time)
+  time_symptoms = $db.execute( <<-SQL
+    SELECT symptom FROM symptoms
+    WHERE time_of_day="#{time}";
+    SQL
+    )
+  puts "_____________________________"
+  puts "Symptoms that occur in the #{time}:"
+  time_symptoms.each do | symptom |
+      puts "#{symptom['symptom']}"
+  end
+  puts "_____________________________"
 end
 
 # def view_severity_based_symptoms(severity)
@@ -40,33 +64,18 @@ end
 #   end
 # end
 
-def view_all_symptoms
-  symptoms = $db.execute("SELECT * FROM symptoms;")
-    symptoms.each do | symptom |
-      puts "#{symptom['id']}."
-      puts "Symptom: #{symptom['symptom']}"
-      puts "Date: #{symptom['date']}"
-      puts "Time of day: #{symptom['time_of_day']}"
-      puts "Severity: #{symptom['severity']}"
-      puts "***********************************************"
-    end
-end
-
-def view_symptoms_time_of_day(time)
-  time_symptoms = $db.execute( <<-SQL
-    SELECT symptom FROM symptoms
-    WHERE time_of_day="#{time}";
+def view_symptoms_specific_date(day)
+    day_symptoms = $db.execute( <<-SQL
+    SELECT * FROM symptoms
+    WHERE date="#{day}";
     SQL
     )
-  puts "*****************************************"
-  puts "Symptoms that occur in the #{time}:"
-  time_symptoms.each do | symptom |
+  puts "_____________________________"
+  puts "Symptoms that occurred on #{day}:"
+  day_symptoms.each do | symptom |
       puts "#{symptom['symptom']}"
   end
-  puts "*****************************************"
-end
-
-def view_symptoms_specific_date
+  puts "_____________________________"
 end
 
 def view_same_symptoms
@@ -109,7 +118,10 @@ end
 
 # View symptoms:
 def view
-  puts "Do you want to view ALL symptoms, or view symptoms based on TIME of day (type 'all', or 'time'):"
+  puts "Do you want to:"
+  puts "view ALL info on all symptoms (type 'all')"
+  puts "view symptoms based on TIME of day (type 'time')"
+  puts "view all symptoms from a certain DAY (type 'day')"
   input = gets.chomp
   if input == 'all'
     view_all_symptoms
@@ -117,6 +129,10 @@ def view
     puts "Which time of day would you like to view symptoms for? (type 'morning', 'afternoon', or 'evening')"
     time = gets.chomp
     view_symptoms_time_of_day(time)
+  elsif input == 'day'
+    puts "For which day would you like information? (type a date in the format 'DD/MM/YYYY')"
+    day = gets.chomp
+    view_symptoms_specific_date(day)
   end
 end
 # Different actions can be:
@@ -125,22 +141,28 @@ end
 # Update: an entry - change the last thing you just added,
 # Delete: an entry - remove the most recent entry
 puts "Welcome to your Symptom Log!"
-puts "Would you like to:"
-puts "--> ADD symptoms (type 'add')"
-puts "--> VIEW symptoms (type 'view')"
-puts "--> EXIT program (type 'exit')"
+# Should main menu be here, as in exiting another function leads back here?
+action = ''
+until action == 'exit'
+    puts "**************************************"
+    puts "MAIN MENU:"
+    puts "Would you like to:"
+    puts "--> ADD symptoms (type 'add')"
+    puts "--> VIEW symptoms (type 'view')"
+    puts "--> EXIT program (type 'exit')"
+    puts "**************************************"
 
-action = gets.chomp
-if action == 'add'
-  add
-elsif action == 'view'
-  view
-elsif action == 'exit'
-  puts "Thank you for using Symptom Log."
-else
-  puts "I didn't understand that."
+    action = gets.chomp
+    if action == 'add'
+      add
+    elsif action == 'view'
+      view
+    elsif action == 'exit'
+      puts "Thank you for using Symptom Log. Goodbye!"
+    else
+      puts "I didn't understand that."
+    end
 end
-
 
 # Update last symptom:
 
@@ -150,12 +172,4 @@ end
 
 # 10.times do
 #   create_kitten(db, Faker::Name.name, 0)
-# end
-
-# explore ORM by retrieving data
-
-# kittens = db.execute("SELECT * FROM kittens;")
-# puts kittens.class
-# kittens.each do | kitten |
-#   puts "#{kitten['name']} is #{kitten['age']}."
 # end
