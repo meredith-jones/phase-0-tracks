23efae7433122e440b1d9d2c316a398b2344e30d
meredith-jones <meredithjw@gmail.com>
2016-09-30 12:15:34 -0700

    Added functionality for adding campuses to a table.

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'sqlite3'
 
 # this is to link the css sort of
-set :public_folder, File.dirname(__FILE__) + '/sta tic'
+set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
@@ -36,4 +36,19 @@ end
 get '/campusresults' do
   @campus = db.execute("SELECT * FROM students WHERE campus=(?)", [params['campus']])
   erb :campusresults
+end
+
+get '/campus/new' do
+  erb :new_campus
+end
+
+post '/campuses' do
+  db.execute("INSERT INTO campuses (campus) VALUES (?)", [params['campus']])
+  redirect '/campus_list'
+end
+
+get '/campus_list' do
+  # puts data into our template:
+  @campuses = db.execute("SELECT * FROM campuses")
+  erb :campus_list
 end
