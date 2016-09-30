5f012e8c1d5ef6619521bff3b3c90508bd4da479
meredith-jones <meredithjw@gmail.com>
2016-09-30 10:46:53 -0700

    Added notes from video to file.

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -2,16 +2,19 @@
 require 'sinatra'
 require 'sqlite3'
 
-set :public_folder, File.dirname(__FILE__) + '/static'
+# this is to link the css sort of
+set :public_folder, File.dirname(__FILE__) + '/sta tic'
 
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
 # show students on the home page
 get '/' do
+  # puts data into our template:
   @students = db.execute("SELECT * FROM students")
   erb :home
 end
+# erb :home accesses html template (erb is a method)
 
 get '/students/new' do
   erb :new_student
@@ -22,6 +25,7 @@ end
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
+  # inserts student into database and redirects to home page
 end
-
+# All 3 methods of sending data to the server end up in params.
 # add static resources
